889ed0ebb897a0bf6a5e99113716b4abd1a64603
Cia <luicencia@mail.hei.school>
2026-05-25 11:18:51 +0300

    feat: design speaker detail page (#3)
    * feat: enhance session and speaker pages with improved layout and data display
    
    * feat: update speaker image URL and enhance layout for speaker detail page
    
    * feat: refactor speaker image display to use Avatar component
    
    ---------
    
    Co-authored-by: Lui100cia <lui100cia@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,5 @@ import { Input } from "@/components/ui/input"
 
 
 export default function HomePage() {
-    return <div className="h-svh w-screen flex items-center justify-center">
-        <Card className="w-full max-w-xl">
-            <CardHeader>
-                <CardTitle>Connecter sur votre compte</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <form className="space-y-4">
-                    <Field>
-                        <FieldLabel>Email</FieldLabel>
-                        <Input type="email" placeholder="email@domain.ltd" />
-                    </Field>
-                    <Field>
-                        <FieldLabel>Password</FieldLabel>
-                        <Input type="password" placeholder="*******" />
-                    </Field>
-                    
-                    <Button className="">Connexion</Button>
-                </form>
-            </CardContent>
-        </Card>
-    </div>
+    return <div>Landing page</div>
 }
--- src/app/speakers/[id]/page.tsx
+++ src/app/speakers/[id]/page.tsx
@@ -1,10 +1,46 @@
+import { BackButton } from "@/components/bloc/back-button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { getInitials } from "@/lib/name";
+import { speakers } from "@/mocks/speaker";
+
 type SpeakersPageProps = {
   params: Promise<{ id: string }>
 }
 
 export default async function SpeakersPage({params}:SpeakersPageProps ) {
     const {id} = await params
+
+    //TODO: fetch speaker data
+    const speaker = speakers.find(s => s.id === id)
+
+    if (!speaker) {
+        return (
+            <h1>Speaker not found</h1>
+        );
+    }
+
     return (
-        <h1>Speakers, {id} </h1>
+        <div className="space-y-4">
+            <BackButton />
+            <Avatar className="size-32">
+                <AvatarImage src={speaker.imageUrl} alt={speaker.name}/>
+                <AvatarFallback>{getInitials(speaker.name)}</AvatarFallback>
+            </Avatar>
+            <h1 className="text-2xl font-bold mt-2">{speaker.name}</h1>
+            <p className="text-muted-foreground">{speaker.bio}</p>
+
+            <div>
+                {speaker.socialLinks.map(link => (
+                    <Button key={link.label} variant="link" asChild>
+                        <a href={link.link} target="_blank">
+                            {link.label}
+                        </a>
+                    </Button>
+                ))}
+            </div>
+
+            
+        </div>
     );
 }

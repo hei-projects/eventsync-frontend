9cdb18710731b5e07beb8018e0f5b85aed5fee24
Miantsa <hei.miantsa.3@gmail.com>
2026-06-13 01:49:26 +0300

    feat: add home page and events pages with live badge

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,20 +1,20 @@
-import { EventCard } from "@/components/event/event-card";
-import { events } from "@/mocks/event";
-import Link from "next/link";
+import { EventCard } from "@/components/event/event-card"
+import { getEvents } from "@/lib/api"
+import Link from "next/link"
 
-export default function EventsPage() {
+export default async function EventsPage() {
+  const events = await getEvents()
+
   return (
-    <div className="p-6">
+    <div>
       <h1 className="text-3xl font-bold mb-6">Événements</h1>
-
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         {events.map((event) => (
-          <Link key={event.id} href={`/events/${event.id}`} >
-            <EventCard  event={event}/>
+          <Link key={event.id} href={`/events/${event.id}`}>
+            <EventCard event={event} />
           </Link>
-          
         ))}
       </div>
     </div>
-  );
+  )
 }
